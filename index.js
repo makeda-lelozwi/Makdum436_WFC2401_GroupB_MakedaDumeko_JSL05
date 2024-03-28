65830db21087f9e86f7edbcd275bfab5b9dc2eef
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-03-28 09:12:32 +0200

    added preferences

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const songs = [
 const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
+    "Drax": "Pop",
+    "Rocket": "Rock",
+    "Groot": "R&B",
     // Add preferences for Drax, Rocket, and Groot
 };
 
